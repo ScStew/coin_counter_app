aa2ab22566d38b4cf5553efab6df1120ee7f68ee
ScStew <scottmstewart2@gmail.com>
2017-08-03 21:08:13 -0400

    added initial login and names.erb

--- app.rb
+++ app.rb
@@ -1,1 +1,22 @@
 require "sinatra"
+
+get '/' do
+erb :login 
+end
+
+post '/login' do
+	correct_login = {"scstew" => "12345"}
+	username = params[:username]
+	password = params[:password]
+		correct_login.each do |key, value|
+			if key == username and value == password
+				redirect '/names'
+			else
+				redirect '/'
+			end
+		end
+end
+
+get '/names' do
+	erb :names
+end

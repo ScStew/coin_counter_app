dc99db1288ac20106871e9a99afacb71daf5fc10
ScStew <scottmstewart2@gmail.com>
2017-08-03 21:41:54 -0400

    added login message and started coins.erb

--- app.rb
+++ app.rb
@@ -1,22 +1,36 @@
 require "sinatra"
 
+
 get '/' do
-erb :login 
+message = params[:message]
+erb :login , locals: {message: message}
 end
 
 post '/login' do
-	correct_login = {"scstew" => "12345"}
+	correct_login = {"scstew" => "12345", "test" => "1234"}
 	username = params[:username]
 	password = params[:password]
 		correct_login.each do |key, value|
 			if key == username and value == password
 				redirect '/names'
-			else
-				redirect '/'
 			end
 		end
+		message = "incorrect username or password"
+		redirect '/?message=' + message
 end
 
 get '/names' do
 	erb :names
+end
+
+post '/name' do
+	fname = params[:fname]
+	lname = params[:lname]
+	redirect '/coin?fname=' + fname + "&lname=" + lname
+end
+
+get '/coin' do
+	fname = params[:fname]
+	lname = params[:lname]
+	erb :coins ,locals:{fname:fname,lname:lname}
 end
